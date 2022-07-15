26f1368da75b9b6d175b7390820017ac7e57b6b4
akshat_jain2 <akshat_jain2@NGL014618.persistent.co.in>
2022-07-15 11:41:25 +0530

    Some changes

--- src/com/training/MyClass.java
+++ src/com/training/MyClass.java
@@ -3,8 +3,8 @@ package com.training;
 public class MyClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Abhay");
+		
+		System.out.println("Hello Abhay ");
 
 	}
 
